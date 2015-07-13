1354a42e2a279459da5e2a8fd7216aa7adba7338
Alexey Zakharov <alexey.zakharov@sarov-itc.ru>
2015-07-13 17:15:17 +0300

    Changed app's port.

--- server.js
+++ server.js
@@ -9,4 +9,8 @@ app.get('/', function(req, res) {
 
 app.use(express.static(__dirname));
 
-app.listen(3000);
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
+
+app.listen(port);
